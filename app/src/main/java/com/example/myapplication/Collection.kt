0a45bee169a56b840a4f0a337d48ef3c65e9fa20
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-10-12 21:16:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+fun Contohlist(){
+    println("=== List ===")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data kedalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape.remove("Triangle")
+
+    // Menambah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapeLocked: List<String> = shape
+    println(shapeLocked)
+
+}
