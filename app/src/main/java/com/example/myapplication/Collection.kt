605e3d32a2c1bc92642a54dce9b2c746cc8d12c7
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-10-12 22:29:05 +0700

    Membuat Fungsi Contoh Map serta mengisi fungsi tersebut

--- app/src/main/java/com/example/myapplication/Collection.kt
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -71,6 +71,27 @@ fun ContohMap() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable
+    val shape: Map<String, Int> = mutableMapOf() Of("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    //Menambahkan data dari Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Mengubah data didalam Map Mutable
+
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
 }
+
 
 
