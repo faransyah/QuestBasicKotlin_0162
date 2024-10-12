525af4a61b64da870ea289da2b5cb7631f3c1e50
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-10-12 23:03:48 +0700

    fungsi ConditionalStatemet dan if else

---
+++ app/src/main/java/com/example/myapplication/ConditionalExpresion.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatemet() {
+    println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }else {
+        println("Negative Number")
+    }
+
+
+
+
+}
