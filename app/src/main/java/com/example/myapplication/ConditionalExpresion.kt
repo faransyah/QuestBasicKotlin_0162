3f20f8b2c225412c8851e516c4fb96104f5e45f1
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-10-12 23:06:41 +0700

    Menambahkan When

--- app/src/main/java/com/example/myapplication/ConditionalExpresion.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpresion.kt
@@ -15,6 +15,20 @@ fun ConditionalStatemet() {
         println("Negative Number")
     }
 
+    // When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Teusday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+
 
 
 
