170d2524ba7de207d9619f20fdd008840a36a8f0
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-10-12 23:15:26 +0700

    menambahkan fungsi pengulangan dan fungsi utama

--- app/src/main/java/com/example/myapplication/ConditionalExpresion.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpresion.kt
@@ -28,8 +28,43 @@ fun ConditionalStatemet() {
         else -> println("Invalid Day")
     }
 
-
-
-
-
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
 }
+
+
+    fun Perulangan() {
+        println()
+        println("=== Perulangan ===")
+
+
+        for (i in 1..5) {
+            println("Perulangan ke-$i")
+        }
+        val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+        for (car in cars) {
+            println("Car type: $car")
+        }
+
+        var x = 1
+        while (x <= 5) {
+            println("Perulangan ke-$x")
+            x++
+        }
+
+        var y = 1
+        do {
+            println("Perulangan ke-$y")
+            y++
+        } while (y <= 5)
+    }
+    fun main() {
+        ConditionalStatemet()
+        Perulangan()
+    }
